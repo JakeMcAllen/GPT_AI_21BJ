91c5583dd3c4f0b5ff3585884c8383f9f31ffa78
21BJ <hello@21bj.io>
2024-08-20 12:17:33 +0200

    add post rest

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/getResponse', methods = ['POST'])
+def login():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        # Call AI
+        response = json["name"]
+        
+        # Create json for output
+        data = {'response': response}
+        # Send output
+        return jsonify(data), 201
+    else:
+        return jsonify({"error": 'Content-Type not supported!'}), 501
+
+
