cd5f2dd1e1c2028c1a310bf5da96a40012c3f3e1
21BJ <hello@21bj.io>
2024-09-10 12:03:30 +0200

    Copy of Agent for import NSP

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+from ai.Agent import agent_routine
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/agent", methods=["POST"])
+def AgentCall():
+    print("In !!!")
+    jsn = request.get_json()
+    print(f"json: {jsn["query"]}")
+    print(f"json: {len(jsn["keyWords"])}")
+
+    if jsn["query"] != "" and len(jsn['keyWords']) > 0:
+        print(jsn["query"])
+        print(jsn['keyWords'][0])
+
+        response = agent_routine(jsn["query"], jsn['keyWords'][0], mode=0)
+
+        return {"status": "true", "response": response}, 201
+
+    return {"status": "fail"}, 411
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+
+
+if __name__ == '__main__':  
+   app.run()
