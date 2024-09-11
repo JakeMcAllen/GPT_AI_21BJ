acc450764f7f772d44bd51e2d21adfbb67cb8901
21BJ <hello@21bj.io>
2024-09-11 10:49:12 +0200

    Update flask main file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-from ai.Agent import agent_routine
+from Agent import agent_routine
 
 
 app = Flask(__name__)
